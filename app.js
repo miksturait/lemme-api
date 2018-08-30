34a3f5a388d1701f017b44cec35589756969ff0f
Michał Staśkiewicz <staskiewicz.miko@gmail.com>
2018-08-30 17:41:18 +0200

    Implement draft of tables endpoint

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const Koa = require('koa');
-const app = new Koa();
+const Router = require('koa-router');
 
-// response
-app.use(ctx => {
-  ctx.body = 'Hello Koa';
+const app = new Koa();
+const router = new Router();
+
+
+
+const setContentType = async (ctx, next) => {
+  await next();
+
+  ctx.type = 'application/vnd.api+json';
+};
+
+router.get('/api/v1/tables', async (ctx, next) => {
+  ctx.body = JSON.stringify({});
+  await next()
 });
+
+app.use(setContentType)
+app.use(router.routes());
 
 module.exports = app;
