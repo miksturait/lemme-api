335f1609f64098b984bafd130bbf45a97b95ffbe
Michał Staśkiewicz <staskiewicz.miko@gmail.com>
2018-09-06 17:56:05 +0200

    Tables create endpoint

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
 const tablesSerializer = require('./tables-serializer');
+const JSONAPIDeserializer = require('jsonapi-serializer').Deserializer;
 const database = require('./database.js');
 const Table = require('./Table');
 
@@ -21,7 +23,22 @@ router.get('/api/v1/tables', async (ctx, next) => {
   await next();
 });
 
-app.use(setContentType)
+router.post('/api/v1/tables', async (ctx, next) => {
+  const deserializeOption = {
+    keyForAttribute: 'camelCase'
+  };
+  const payload = await new JSONAPIDeserializer(deserializeOption)
+    .deserialize(ctx.request.body);
+
+  const newTable = await Table.create(payload);
+  const response = tablesSerializer.serialize(newTable);
+
+  ctx.body = JSON.stringify(response)
+  await next();
+});
+
+app.use(bodyParser());
+app.use(setContentType);
 app.use(router.routes());
 
 module.exports = app;
