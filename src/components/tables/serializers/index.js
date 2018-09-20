98835bbb3e2de6c27ab3a28d07d33f422fefd775
Michał Staśkiewicz <staskiewicz.miko@gmail.com>
2018-09-20 18:33:39 +0200

    Add nested resource attributes

--- src/components/tables/serializers/index.js
+++ src/components/tables/serializers/index.js
@@ -4,6 +4,7 @@ const tablesSerializer = new JSONAPISerializer('tables', {
   attributes: ['name', 'seatsCount', 'seats'],
   seats: {
     ref: '_id',
+    attributes: ['status']
   },
   topLevelLinks: {
     self: 'http://localhost:3000/api/v1/tables'
