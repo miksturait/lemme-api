8162541f9f4793dc9610817eaad7b55e236ca93b
Robert Mrowiec <rmrowiec@icloud.com>
2018-10-04 18:59:54 +0200

    update with broadcast for everyone

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const Koa = require('koa');
 const tables = require('./src/components/tables');
 const logger = require('koa-logger')
 const cors = require('@koa/cors');
-const webSocket = require('./src/components/chat/index')
+const io = require('socket.io')(3001);
 const app = new Koa();
 
 const setContentType = async (ctx, next) => {
@@ -16,6 +16,12 @@ app.use(cors());
 app.use(logger());
 app.use(setContentType);
 app.use(tables.routes);
-app.use(webSocket)
+
+io.on('connection', (socket) => {
+  console.log('connected')
+  socket.on('message', (msg) => {
+      io.sockets.emit('User connected');
+  });
+});
 
 module.exports = app;
--- src/components/chat/index.js
+++ src/components/chat/index.js
@@ -4,12 +4,3 @@ const io = require('socket.io')(3001);
 module.exports =  {
   routes: chatRouter.routes()
 }
-
-module.exports = () => {
-  io.on('connection', (socket) => {
-    console.log('connected')
-    socket.on('message', (msg) => {
-        socket.broadcast.emit('User connected');
-    });
-  });
-}
