c63f8f2d4f7cbff16e5e76af181a9bd726e0fa6b
Robert Mrowiec <rmrowiec@icloud.com>
2018-11-08 17:34:37 +0100

    add userName

--- app.js
+++ app.js
@@ -19,8 +19,16 @@ app.use(tables.routes);
 
 io.on('connection', (socket) => {
   console.log('connected')
-  socket.on('message', (msg) => {
-      io.sockets.emit('User connected');
+  
+  socket.on('message', (message) => {
+    let split = socket.request.headers.cookie.replace('; ', '=').split('=')
+    let index = split.indexOf('socketUserName')
+    let userName = split[index+1]
+  
+    io.sockets.emit({
+      message,
+      userName
+    })
   });
 });
 
